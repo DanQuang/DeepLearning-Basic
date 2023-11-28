0fb4aca8f36a31d9300a7541d9ab4a710e58a443
DanQuang <21521917@gm.uit.edu.vn>
2023-11-29 01:58:12 +0700

    update config and model

--- model/LeNet.py
+++ model/LeNet.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+# Custom LeNet using ReLU
 class LeNet(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -11,25 +12,26 @@ class LeNet(nn.Module):
 
         self.block_1 = nn.Sequential(
             nn.Conv2d(in_channels= self.image_C, out_channels= 6, kernel_size= 5, padding= 2),
-            nn.Sigmoid(),
+            nn.ReLU(),
             nn.AvgPool2d(kernel_size= 2, stride= 2)
         )
 
         self.block_2 = nn.Sequential(
             nn.Conv2d(in_channels= 6, out_channels= 16, kernel_size= 5, padding= 0),
-            nn.Sigmoid(),
+            nn.ReLU(),
             nn.AvgPool2d(kernel_size= 2, stride= 2)
         )
 
         self.dense = nn.Sequential(
             nn.Flatten(),
             nn.LazyLinear(out_features= 120),
-            nn.Sigmoid(),
+            nn.ReLU(),
             nn.LazyLinear(out_features= 84),
-            nn.Sigmoid(),
+            nn.ReLU(),
             nn.LazyLinear(out_features= self.num_classes)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x.unsqueeze(dim = 1)
+        if (x.dim() == 3):
+            x = x.unsqueeze(dim = 1)
         return self.dense(self.block_2(self.block_1(x)))
