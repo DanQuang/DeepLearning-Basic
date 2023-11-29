35fe1a7fc60b7c6cf715fe400382d944ca9c5cb2
DanQuang <21521917@gm.uit.edu.vn>
2023-11-29 08:59:07 +0700

    update evaluate

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -1,10 +1,12 @@
 from torchmetrics import F1Score, Accuracy, Precision, Recall
+import torch
 
 def compute_score(num_classes, y_true, y_pred):
-    Acc_fn = Accuracy(task= "multiclass", num_classes= num_classes, average= 'macro')
-    Prec_fn = Precision(task= "multiclass", num_classes= num_classes, average= 'macro')
-    Recall_fn = Recall(task= "multiclass", num_classes= num_classes, average= 'macro')
-    F1_score = F1Score(task= "multiclass", num_classes= num_classes, average= 'macro')
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    Acc_fn = Accuracy(task= "multiclass", num_classes= num_classes, average= 'macro').to(device)
+    Prec_fn = Precision(task= "multiclass", num_classes= num_classes, average= 'macro').to(device)
+    Recall_fn = Recall(task= "multiclass", num_classes= num_classes, average= 'macro').to(device)
+    F1_score = F1Score(task= "multiclass", num_classes= num_classes, average= 'macro').to(device)
 
     acc = Acc_fn(y_true, y_pred)
     prec = Prec_fn(y_true, y_pred)
