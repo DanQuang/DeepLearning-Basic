e9bbe0678657f2f34ea9529ff17ee127e7805455
DanQuang <21521917@gm.uit.edu.vn>
2023-12-26 01:35:29 +0700

    update

--- data_utils/load_data.py
+++ data_utils/load_data.py
@@ -1,5 +1,5 @@
 from data_utils import utils
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 import torch
 import idx2numpy
 
@@ -13,18 +13,19 @@ class MNISTDataset(Dataset):
         imgs = torch.tensor(imgs, dtype= torch.float32)
         labels = torch.tensor(labels, dtype=torch.long)
     
-        self.__data = [{"image": img ,"label": label} for img, label in zip(imgs, labels)]
+        self.__data = {}
+
+        for idx, (img, label) in enumerate(zip(imgs, labels)):
+            self.__data[idx] = {
+                "image": img,
+                "label": label
+            }
 
     def __len__(self):
         return len(self.__data)
     
     def __getitem__(self, index):
-        if isinstance(index, slice):
-            start, stop, step = index.start or 0, index.stop or len(self), index.step or 1
-            selected_data = [{"image": self.__data[i]["image"], "label": self.__data[i]["label"]} for i in range(start, stop, step)]
-            return selected_data
-        else:
-            return self.__data[index]
+        return self.__data[index]
 
 class Load_Data:
     def __init__(self, config):
@@ -41,9 +42,10 @@ class Load_Data:
 
     def load_train_dev(self):
         train_dataset = MNISTDataset(self.train_img_path, self.train_label_path)
-        idx = int(0.8*len(train_dataset))
-        dev_dataset = train_dataset[idx:]
-        train_dataset = train_dataset[:idx]
+        train_size = int(0.8*len(train_dataset))
+        dev_size = len(train_dataset) - train_size
+        train_dataset, dev_dataset = random_split(train_dataset, [train_size, dev_size])
+
 
         train_dataloader = DataLoader(train_dataset, self.train_batch, shuffle= True, collate_fn= utils.collate_fn)
         dev_dataloader = DataLoader(dev_dataset, self.test_batch, shuffle= True, collate_fn= utils.collate_fn)
